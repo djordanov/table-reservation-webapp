b51df2341c15de086a23792408624d876afbe09d
Damian Jordanov <d.jordanov@posteo.net>
2018-11-04 20:00:34 +0100

    shell of 'view reservation' site

--- src/app/reservation-details/reservation-details.component.ts
+++ src/app/reservation-details/reservation-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+
+import { Reservation } from '../Reservation';
+import { ReservationService } from '../reservation.service';
 
 @Component({
   selector: 'app-reservation-details',
@@ -6,4 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./reservation-details.css']
 })
 export class ReservationDetailsComponent {
+  reservation: Reservation;
+
+  constructor(private reservationService: ReservationService) {}
+
+  getReservation(id: String): void {
+    alert('reservation id: ' + id);
+    this.reservationService.getReservation(id)
+      .subscribe(reservation => this.reservation = reservation);
+  }
 }
