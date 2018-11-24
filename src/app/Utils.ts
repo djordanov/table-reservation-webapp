a4c1f89f697eda5ed37b737e4b31ce73b0f9d1fd
Damian Jordanov <d.jordanov@posteo.net>
2018-11-24 12:09:09 +0100

    improved error handling/ error messages

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -11,16 +11,16 @@ const parseReservationResponse = (reservationResponse: any): Reservation => {
   // reservation doesn't exist
   if (
     reservationResponse.result === 'error' &&
-    reservationResponse.text === 'Falsche RES_ID'
+    reservationResponse.text === 'Falsche RES_PID'
   ) {
-    throw new Error('Reservation doesn\'t exist');
+    throw new Error('Reservierungsnummer nicht gefunden');
   }
   // reservation was cancelled
   if (
     reservationResponse.result === 'error' &&
     reservationResponse.text === 'Durch Kunde storniert'
   ) {
-    throw new Error('Reservation was cancelled');
+    throw new Error('Reservierung wurde bereits storniert');
   }
 
   const reservation = reservationResponse.info_res;
