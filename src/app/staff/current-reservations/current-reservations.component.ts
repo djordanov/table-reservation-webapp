9ee3d5915365991d64b0e9906d6ee7ccb3a12446
Damian Jordanov <d.jordanov@posteo.net>
2018-11-25 10:20:12 +0100

    now has a date picker form, so staff can look at other dates too

--- src/app/staff/current-reservations/current-reservations.component.ts
+++ src/app/staff/current-reservations/current-reservations.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
 import { Table } from 'src/app/data-models/Table';
 import { TableService } from 'src/app/services/table.service';
 
 import { parseTablesResponse } from '../../Utils';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-current-reservations',
@@ -10,16 +12,27 @@ import { parseTablesResponse } from '../../Utils';
   styleUrls: ['./current-reservations.component.css']
 })
 export class StaffCurrentReservationsComponent implements OnInit {
+  form = this.fb.group({
+    date: formatDate(new Date(), 'yyyy-MM-dd', 'en_US'),
+    time: formatDate(new Date(), 'mm:HH:ss', 'en_US')
+  });
   tables: Table[];
 
-  constructor(private tableService: TableService) {}
+  constructor(private tableService: TableService, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.fetchReservations();
   }
 
+  onSubmit() {
+    this.fetchReservations();
+  }
+
   fetchReservations() {
-    this.tableService.getCurrentTableStatus().subscribe(response => {
+    const datestr =
+      this.form.get('date').value + 'T' + this.form.get('time').value;
+    const date = new Date(datestr);
+    this.tableService.getTableStatus(date).subscribe(response => {
       const tables = parseTablesResponse(response);
       this.tables = tables.filter(table => table.res);
     });
