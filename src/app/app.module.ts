b3fd65c2a2ac01c2734d25fe3377fd1e137a13f0
Damian Jordanov <d.jordanov@posteo.net>
2018-11-27 23:38:16 +0100

    added ng-bootstrap for modals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AlertModule } from 'ngx-bootstrap';
-
-import { ModalModule } from 'ngx-modialog';
-import { BootstrapModalModule } from 'ngx-modialog/plugins/bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ReservationCreateComponent } from './reservation-create/reservation-create.component';
 import { ReservationDeleteComponent } from './reservation-delete/reservation-delete.component';
 import { ReservationDetailsComponent } from './reservation-details/reservation-details.component';
 import { ReservationMenuComponent } from './reservation-menu/reservation-menu';
 import { TablesComponent } from './templates/tables/tables.component';
-import { FormsModule } from '@angular/forms';
 
 import { StaffMenuComponent } from './staff/menu/staff-menu.component';
 import { StaffCurrentReservationsComponent } from './staff/current-reservations/current-reservations.component';
-import { CommonModule } from '@angular/common';
 import { ReservationCreateResponseComponent } from './reservation-create-response/reservation-create-response';
 import { HeaderComponent } from './templates/header/header.component';
 import { CancelModalComponent } from './cancel-modal/cancel-modal.component';
@@ -43,14 +40,13 @@ import { StaffBookingPlanComponent } from './staff/staff-booking-plan/staff-book
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AlertModule.forRoot(),
-    ModalModule.forRoot(),
-    BootstrapModalModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    CommonModule
+    CommonModule,
+    NgbModule.forRoot()
   ],
+  entryComponents: [CancelModalComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
