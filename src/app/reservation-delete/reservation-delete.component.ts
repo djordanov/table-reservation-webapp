25cc56d2e2b87396ced12ff410bd6718c96ca2e2
Damian Jordanov <d.jordanov@posteo.net>
2018-11-30 18:05:24 +0100

    added reservation cancellation confirmation modal. It actually works now

--- src/app/reservation-delete/reservation-delete.component.ts
+++ src/app/reservation-delete/reservation-delete.component.ts
@@ -17,7 +17,6 @@ import { CancelModalComponent } from '../cancel-modal/cancel-modal.component';
 export class ReservationDeleteComponent {
   error: Error;
   openConfirm: boolean;
-  reservation: Reservation;
   form = this.fb.group({
     reservationNumber: ['Reservierungsnummer', Validators.required]
   });
@@ -29,19 +28,20 @@ export class ReservationDeleteComponent {
   ) {}
 
   onSubmit(): void {
-    const modalRef = this.modalService.open(CancelModalComponent);
-    modalRef.componentInstance.name = 'World';
-
-    // this.onCancel(this.form.get('reservationNumber').value);
+    this.onCancel(this.form.get('reservationNumber').value);
   }
 
+  /**
+   * fetch reservation and open reservation cancellation modal
+   */
   onCancel(reservationNumber: String): void {
     this.reservationService
       .getReservation(reservationNumber)
       .subscribe(reservationResponse => {
         try {
-          this.reservation = parseReservationResponse(reservationResponse);
-          this.openConfirm = true;
+          const reservation = parseReservationResponse(reservationResponse);
+          const modalRef = this.modalService.open(CancelModalComponent);
+          modalRef.componentInstance.reservation = reservation;
         } catch (err) {
           this.error = err;
         }
